062f7927b1dabab9a106579d065540a8d0c61eba
aluno <aluno@192.168.0.134>
2024-11-29 10:37:47 -0300

    SEGUNDO COMMIT

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -4,11 +4,18 @@ public class Executavel {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
-		System.out.println("Meu nome é Eduardo")
+		System.out.println("Meu nome é Eduardo");
 		int inteiro;
 		double numeroDecimal;
 		String texto;
 		//Atribuição de valores
+		inteiro = 5;
+		numeroDecimal = 5.5;
+		texto = "Eduardo";
+		
+		System.out.println("Nome: "+ texto);
+		System.out.println("Numero inteiro: "+ inteiro);
+		System.out.println("Numero decimal: "+ numeroDecimal);
 		
 	} 
 
